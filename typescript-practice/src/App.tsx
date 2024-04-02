51833603d25f686405e15de64818ded5838d17a3
HakeemAlavi <hakeem.alavi@strathmore.edu>
2024-04-02 17:51:27 +0300

    feat: add React to input with state

--- typescript-practice/src/App.tsx
+++ typescript-practice/src/App.tsx
@@ -1,27 +1,71 @@
 // App.js
 import './App.css';
-import React, { useState, ButtonHTMLAttributes } from 'react';
+import { useState, ChangeEvent, FormEvent } from 'react';
 
-export default function MyApp() {
-  const [count, setCount] = useState(0);
+export default function Form() {
+  const [answer, setAnswer] = useState<string>('');
+  const [error, setError] = useState<Error | null>(null);
+  const [status, setStatus] = useState<'typing' | 'submitting' | 'success'>('typing');
 
-  function handleClick() {
-    setCount(count + 1);
+  if (status === 'success') {
+    return <h1>That's right!</h1>;
+  }
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setStatus('submitting');
+    try {
+      await submitForm(answer);
+      setStatus('success');
+    } catch (err) {
+      setStatus('typing');
+      setError(err as Error); // Cast err to Error type
+    }
+  }
+
+  function handleTextareaChange(e: ChangeEvent<HTMLTextAreaElement>) {
+    setAnswer(e.target.value);
   }
 
   return (
-    <div>
-      <h1>Counters that update together</h1>
-      <MyButton count={count} onClick={handleClick} />
-      <MyButton count={count} onClick={handleClick} />
-    </div>
+    <>
+      <h2>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === 'submitting'}
+        />
+        <br />
+        <button disabled={
+          answer.length === 0 ||
+          status === 'submitting'
+        }>
+          Submit
+        </button>
+        {error !== null &&
+          <p className="Error">
+            {error.message}
+          </p>
+        }
+      </form>
+    </>
   );
 }
 
-function MyButton({ count, onClick }: { count: number; onClick: ButtonHTMLAttributes<HTMLButtonElement>['onClick'] }) {
-  return (
-    <button onClick={onClick}>
-      Clicked {count} times
-    </button>
-  );
+function submitForm(answer: string): Promise<void> { // Specify Promise<void> for resolved value
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldError = answer.toLowerCase() !== 'lima';
+      if (shouldError) {
+        reject(new Error('Good guess but a wrong answer. Try again!'));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
 }
